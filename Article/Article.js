2cdaef9688af29ed20b9709fe6370cb14e101500
Joel Bartlett <joel@murbar.com>
2019-01-30 16:42:20 -0500

    Toggle expand button text contnet

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,20 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    const articleClasses = this.domElement.classList;
+    articleClasses.toggle('article-open');
+    if (articleClasses.contains('article-open')) {
+      this.expandButton.textContent = 'Click to Close';
+    } else {
+      this.expandButton.textContent = 'Click to Expand';
+    }
   }
 }
 
